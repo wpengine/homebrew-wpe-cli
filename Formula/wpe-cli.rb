6170840f6f180e6aeeda97306d5a00e34a05a448
goreleaserbot <goreleaser@carlosbecker.com>
2018-12-03 16:18:06 -0500

    Brew formula update for wpe-cli version v0.7.7

--- Formula/wpe-cli.rb
+++ Formula/wpe-cli.rb
@@ -1,9 +1,9 @@
 class WpeCli < Formula
   desc "WP Engine Command Line Interface"
   homepage "https://wpengine.com"
-  url "https://s3-us-east-2.amazonaws.com/wpeclibinaries/wpe-cli/v0.7.6/wpe-cli_0.7.6_darwin_amd64.tar.gz"
-  version "0.7.6"
-  sha256 "92177753ca2d95e53441d5425630c844c6c1d3011a88ea23d599acb69e728f62"
+  url "https://s3-us-east-2.amazonaws.com/wpeclibinaries/wpe-cli/v0.7.7/wpe-cli_0.7.7_darwin_amd64.tar.gz"
+  version "0.7.7"
+  sha256 "376a0dd76c9a11db06d9b5549f91d26c2f946510223166a5dcbd4d84bd7f4182"
 
   def install
     bin.install "wpe"
