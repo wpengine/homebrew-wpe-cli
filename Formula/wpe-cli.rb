cbc042f48ddc8aea390b3e848b98447c8d4e5d0c
goreleaserbot <goreleaser@carlosbecker.com>
2020-03-11 09:27:13 -0500

    Brew formula update for wpe-cli version v0.16.8

--- Formula/wpe-cli.rb
+++ Formula/wpe-cli.rb
@@ -2,16 +2,16 @@
 class WpeCli < Formula
   desc "WP Engine DevKit Command Line Interface"
   homepage "https://wpengine.com"
-  version "0.16.7"
+  version "0.16.8"
   bottle :unneeded
 
   if OS.mac?
-    url "https://s3-us-east-2.amazonaws.com/wpeclibinaries/wpe-cli/v0.16.7/wpe-cli_0.16.7_darwin_amd64.tar.gz"
-    sha256 "221eac02ecb29be0a76a057d0744c2f9979541c8b2f589e3d6bf40223bd22d67"
+    url "https://s3-us-east-2.amazonaws.com/wpeclibinaries/wpe-cli/v0.16.8/wpe-cli_0.16.8_darwin_amd64.tar.gz"
+    sha256 "2052b6105cb63dc37e15c1a3f3fd1fd544faa5ee73ee2261aed25b910a4039b1"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://s3-us-east-2.amazonaws.com/wpeclibinaries/wpe-cli/v0.16.7/wpe-cli_0.16.7_linux_amd64.tar.gz"
-      sha256 "2e13a4fc6f7611a09bfc58e0a28eb07ba63614cea60e0ee3d1e48f7733a0ac97"
+      url "https://s3-us-east-2.amazonaws.com/wpeclibinaries/wpe-cli/v0.16.8/wpe-cli_0.16.8_linux_amd64.tar.gz"
+      sha256 "bc3b84948942ce39bd9322cf096fd0efd87c7f7670f6a33148cd155f7643da83"
     end
   end
   
